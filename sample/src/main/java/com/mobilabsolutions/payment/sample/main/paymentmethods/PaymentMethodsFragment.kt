d588f49bf2d4bdafbff93dcda678ecb7de65f496
yisuk <yisuk@mobilabsolutions.com>
2019-04-15 18:04:44 +0200

    Init the story.

--- sample/src/main/java/com/mobilabsolutions/payment/sample/main/paymentmethods/PaymentMethodsFragment.kt
+++ sample/src/main/java/com/mobilabsolutions/payment/sample/main/paymentmethods/PaymentMethodsFragment.kt
@@ -1,8 +1,13 @@
 package com.mobilabsolutions.payment.sample.main.paymentmethods
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
 import com.mobilabsolutions.payment.sample.core.BaseFragment
+import com.mobilabsolutions.payment.sample.databinding.FragmentPaymentMethodsBinding
 import javax.inject.Inject
 
 /**
@@ -13,6 +18,13 @@ class PaymentMethodsFragment : BaseFragment() {
     lateinit var paymentMethodsViewModelFactory: PaymentMethodsViewModel.Factory
 
     private val viewModel: PaymentMethodsViewModel by fragmentViewModel()
+    private lateinit var binding: FragmentPaymentMethodsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentPaymentMethodsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
 
     override fun invalidate() {
         withState(viewModel) {
