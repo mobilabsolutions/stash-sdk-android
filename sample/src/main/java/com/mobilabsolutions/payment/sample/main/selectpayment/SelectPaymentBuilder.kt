3739bae6964370284604e71ed57d333808473c95
Biju Parvathy <biju@mobilabsolutions.com>
2019-05-10 14:11:02 +0200

    Update styles and selectors

---
+++ sample/src/main/java/com/mobilabsolutions/payment/sample/main/selectpayment/SelectPaymentBuilder.kt
@@ -0,0 +1,13 @@
+package com.mobilabsolutions.payment.sample.main.selectpayment
+
+import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+/**
+ * @author <a href="yisuk@mobilabsolutions.com">Yisuk Kim</a> on 08-04-2019.
+ */
+@Module
+abstract class SelectPaymentBuilder {
+   // @ContributesAndroidInjector
+    // abstract fun bindPaymentFragment(): PaymentMethodsFragment
+}
