20938592837365a866e10c326b30c0619670cf81
Biju Parvathy <biju@mobilabsolutions.com>
2019-05-31 10:18:54 +0200

    Spotless ...

--- sample/src/main/java/com/mobilabsolutions/payment/sample/main/checkout/CheckoutFragment.kt
+++ sample/src/main/java/com/mobilabsolutions/payment/sample/main/checkout/CheckoutFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
-import androidx.navigation.Navigation
 import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.withState
 import com.google.android.material.snackbar.Snackbar
