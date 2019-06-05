6913a6e0796c187fa17f9e266722cf913b6b2c01
Ugljesa Jovanovic <ugljesa@mobilabsolutions.com>
2019-06-05 13:40:18 +0200

    Updated docs for PaymentMethodType and UiCustomizationManager

--- lib/src/main/java/com/mobilabsolutions/payment/android/psdk/PaymentMethodType.kt
+++ lib/src/main/java/com/mobilabsolutions/payment/android/psdk/PaymentMethodType.kt
@@ -1,7 +1,19 @@
 package com.mobilabsolutions.payment.android.psdk
 
+/**
+ * Represents the payment method types the SDK currently supports
+ */
 enum class PaymentMethodType {
+    /**
+     * Credit card
+     */
     CC,
+    /**
+     * SEPA bank account
+     */
     SEPA,
+    /**
+     * PayPal account
+     */
     PAYPAL
 }
