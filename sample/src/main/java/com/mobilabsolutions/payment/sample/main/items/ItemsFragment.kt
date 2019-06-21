75e71b3f8ab7ff76945dc6b40706e2c80408b19b
Biju Parvathy <biju@mobilabsolutions.com>
2019-06-21 18:06:05 +0200

    spotless

--- sample/src/main/java/com/mobilabsolutions/payment/sample/main/items/ItemsFragment.kt
+++ sample/src/main/java/com/mobilabsolutions/payment/sample/main/items/ItemsFragment.kt
@@ -27,7 +27,6 @@ class ItemsFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_items, container, false)
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
