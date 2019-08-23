5104caf21376c0b6d7344401ecd616c1f8bb7638
yisuk <yisuk@mobilabsolutions.com>
2019-08-23 01:24:54 +0100

    Kotlin 1.3.50

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -72,7 +72,7 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.3.41"
+        private const val version = "1.3.50"
         val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
